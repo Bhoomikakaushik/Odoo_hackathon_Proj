2ba1758ab744abae097bed14700eec4e24bf369d
dikshaWagh <dikshawagh57@gmail.com>
2025-07-12 15:50:33 +0530

    update

--- oddo_front/src/App.jsx
+++ oddo_front/src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from './components/Homepage/Homepage.jsx'
 import Login from './components/Login/Login.jsx'
 import { Routes, Route } from 'react-router-dom'
 import SignupForm from './components/SignUp/Signup.jsx';
+import InvitationCard from './components/InvitationCard.jsx/InvitationCard.jsx'
 
 function App() {
 
@@ -16,6 +17,11 @@ function App() {
             <Route path='/' element={<Homepage />} />
             <Route path='/Login' element={<Login />} />
             <Route path='/Signup' element={<SignupForm/>}></Route>
+            <Route path='/Invitations' element={<InvitationCard/>}></Route>
+
+
+            
+            
           </Routes>
         </main>
       </div>
