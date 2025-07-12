2ee6ddb1aa7d29c98191e85b84710334879767cf
dikshaWagh <dikshawagh57@gmail.com>
2025-07-12 10:29:13 +0530

    main

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      
+      <div>
+        <input type="text"
+          placeholder='Enter your name'
+        ></input>
+      </div>
+
     </div>
   )
 }
