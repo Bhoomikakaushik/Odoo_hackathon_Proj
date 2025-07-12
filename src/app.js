f224c71efd2f6f0b560790b957e859fd33e537bc
Bhoomikakaushik <151210021+Bhoomikakaushik@users.noreply.github.com>
2025-07-12 10:59:48 +0530

    setting backend

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose"
+const app = express();
+
+// app.set("port",(process.env.PORT || 8000))
+
+
+// app.get("/",(req, res) => {
+//     return "odoooo"
+// })
+
+// app.listen(port,(req,res) => {
+//     return res.json(`app is listening on port ${port}`)
+// })
