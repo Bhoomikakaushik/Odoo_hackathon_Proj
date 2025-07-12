9398e4e1024d563aa47694648a05c6ef2edc8ad1
Rudrakhsa Dwivedi <146867502+Rudraksha-007@users.noreply.github.com>
2025-07-12 11:56:25 +0530

    Setting up backend

---
+++ oddo_backend/app.js
@@ -0,0 +1,14 @@
+import express from "express"
+import mongoose from "mongoose"
+
+const app = express();
+app.use(express.json())
+const port = process.env.PORT || 8000
+
+app.listen(port,(req,res) => {
+    console.log(`app is listening on port ${port}`)
+})
+
+app.get("/", (req,res) => {
+    res.send("hellloooo!")
+})
